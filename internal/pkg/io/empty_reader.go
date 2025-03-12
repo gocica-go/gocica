ec2b0b3e2ee4d0ca444e81a4f27a9f7363d34a13
mazrean <shunsuke.w04@gmail.com>
2025-03-12 18:30:52 +0900

    Added Seek method to EmptyReader

--- internal/pkg/io/empty_reader.go
+++ internal/pkg/io/empty_reader.go
@@ -2,10 +2,14 @@ package io
 
 import "io"
 
-var EmptyReader io.Reader = &emptyReader{}
+var EmptyReader io.ReadSeeker = &emptyReader{}
 
 type emptyReader struct{}
 
 func (e *emptyReader) Read([]byte) (n int, err error) {
 	return 0, io.EOF
+}
+
+func (e *emptyReader) Seek(int64, int) (int64, error) {
+	return 0, nil
 }
