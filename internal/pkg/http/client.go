222a232f0662e85eede876ef70860ff29d69ee40
mazrean <shunsuke.w04@gmail.com>
2025-03-15 18:09:59 +0900

    HTTP client: enhance transport settings for improved connection management

---
+++ internal/pkg/http/client.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+const (
+	maxIdleConns        = 100000
+	maxIdleConnsPerHost = 10000
+	timeout             = 30 * time.Second
+	keepAliveTime       = 1 * time.Hour
+)
+
+func NewClient() *http.Client {
+	defaultTransport, ok := http.DefaultTransport.(*http.Transport)
+	if !ok {
+		return http.DefaultClient
+	}
+	newTransport := defaultTransport.Clone()
+
+	newTransport.MaxIdleConns = maxIdleConns
+	newTransport.MaxIdleConnsPerHost = maxIdleConnsPerHost
+	newTransport.DialContext = (&net.Dialer{
+		Timeout:   timeout,
+		KeepAlive: keepAliveTime,
+	}).DialContext
+
+	http.DefaultTransport = newTransport
+
+	return &http.Client{
+		Transport: newTransport,
+	}
+}
